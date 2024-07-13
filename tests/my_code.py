5db041b587e7533896d0d9761c0aba6ed95ee3f8
Adam Pawełek <adam.pwk@outlook.com>
2024-07-13 21:33:09 +0200

    added tests

--- tests/my_code.py
+++ tests/my_code.py
@@ -5,7 +5,7 @@ from simpleaitranslator.translator import get_text_language, translate
 
 #print(os.environ.get("OPENAI_API_KEY"))
 
-simpleaitranslator.translator.set_openai_api_key(None)
+#simpleaitranslator.translator.set_openai_api_key(None)
 
 simpleaitranslator.translator.set_openai_api_key(os.environ.get("OPENAI_API_KEY"))
 print(get_text_language("jak ty się nazywasz"))
